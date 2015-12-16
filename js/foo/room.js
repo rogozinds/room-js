28999653492f506842ce489b8f250a03f304b82f
rogozinds <rogozinds@gmail.com>
2015-12-17 00:04:11 +0200

    Working example for 1 item in a list

--- js/foo/room.js
+++ js/foo/room.js
@@ -1,38 +1,50 @@
 window.onload = function() {
     var c = document.getElementById("myCanvas");
-    var context = c.getContext("2d");
-    var background=new Image();
-    background.src = "/resources/pic1.png";
-    background.width=240;
-    background.height=300;
-    if (background.complete) {
-        context.drawImage(background, 0, 0,240,300);
-    } else {
-        background.onload = function () {
-            context.drawImage(background, 0, 0,240,300);
-        };
-    }
+    c.style.width=300+"px";
+    c.style.height=300+"px";
+    c.style.backgroundImage="url('/resources/pic1.png')";
 }
 
+var pos;
 function allowDrop(ev) {
     ev.preventDefault();
 }
-
+function get_pos(ev){
+    pos = [ev.pageX, ev.pageY];
+}
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("Text",ev.target.id);
 }
 
+function getPosition(element) {
+    var xPosition = 0;
+    var yPosition = 0;
+
+    while(element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+    return { x: xPosition, y: yPosition };
+}
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    var x=ev.clientX;
-    var y=ev.clientY;
+    var offset = ev.dataTransfer.getData("text/plain").split(',');
+    var data=ev.dataTransfer.getData("Text");
 
+    var img = document.getElementById("img1");
+    var dx = pos[0] - getPosition(img)["x"];
+    var dy = pos[1] - getPosition(img)["y"];
+
+
+    var list=img.parentElement.parentElement;
+    var item=img.parentElement;
     var c = document.getElementById("myCanvas");
-    var context = c.getContext("2d");
 
-    var background=new Image();
-    background.src = "/resources/dummy.png";
-    context.drawImage(background, x, y,40,40);
+    c.appendChild(img);
+    list.removeChild(item);
+    document.getElementById("img1").style.position="absolute";
+    document.getElementById("img1").style.left=(ev.pageX-dx) +'px';
+    document.getElementById("img1").style.top=(ev.pageY-dy)+'px';
+
 }
-
